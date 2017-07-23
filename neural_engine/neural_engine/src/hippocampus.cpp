f44f30b7e5e4257f3e553854fde92b8b1e378e27
nmsutton <Nate Sutton>
2017-07-23 17:38:39 -0400

    grid cell arrays added

--- neural_engine/neural_engine/src/hippocampus.cpp
+++ neural_engine/neural_engine/src/hippocampus.cpp
@@ -1,10 +1,27 @@
 #include "hippocampus.h"
 
+using namespace std;
+
 hippocampus::hippocampus()
 {
+	double** new_pop;
+	double*** grid_cell_populations = new double**[GRID_POPULATION_SIZE];
+	for (int i = 0; i < GRID_POPULATION_NUMBER; i++) {
+		new_pop = create_grid_population();
+		grid_cell_populations[i] = new_pop;
+	}
 }
 
 hippocampus::~hippocampus()
 {
 }
 
+double** hippocampus::create_grid_population()
+{
+	double** grid_population = new double*[GRID_POPULATION_SIZE];
+	for (int i = 0; i < GRID_POPULATION_SIZE; i++) {
+		grid_population[i] = new double[GRID_POPULATION_SIZE];
+	}
+
+	return grid_population;
+}
--- neural_engine/neural_engine/src/hippocampus.h
+++ neural_engine/neural_engine/src/hippocampus.h
@@ -4,8 +4,14 @@
 class hippocampus
 {
 public:
+	struct grid_cells {
+		double V;
+	};
+	int GRID_POPULATION_NUMBER = 6;
+	int GRID_POPULATION_SIZE = 25;
     hippocampus();
     ~hippocampus();
+    double** create_grid_population();
 
 };
 
