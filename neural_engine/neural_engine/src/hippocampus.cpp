26669f5affa1cfd5d040714d7b551fd2bb514488
nmsutton <Nate Sutton>
2017-07-23 23:56:32 -0400

    grid cell struct implemented

--- neural_engine/neural_engine/src/hippocampus.cpp
+++ neural_engine/neural_engine/src/hippocampus.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 hippocampus::hippocampus()
 {
-	double** new_pop;
-	double*** grid_cell_populations = new double**[GRID_POPULATION_SIZE];
+	grid_cells** new_pop;
+	grid_cells*** grid_cell_populations = new grid_cells**[GRID_POPULATION_SIZE];
 	for (int i = 0; i < GRID_POPULATION_NUMBER; i++) {
 		new_pop = create_grid_population();
 		grid_cell_populations[i] = new_pop;
@@ -16,12 +16,13 @@ hippocampus::~hippocampus()
 {
 }
 
-double** hippocampus::create_grid_population()
+hippocampus::grid_cells** hippocampus::hippocampus::create_grid_population()
 {
-	double** grid_population = new double*[GRID_POPULATION_SIZE];
+	grid_cells** grid_population = new grid_cells*[GRID_POPULATION_SIZE];
 	for (int i = 0; i < GRID_POPULATION_SIZE; i++) {
-		grid_population[i] = new double[GRID_POPULATION_SIZE];
+		grid_population[i] = new grid_cells[GRID_POPULATION_SIZE];
 	}
 
 	return grid_population;
 }
+
--- neural_engine/neural_engine/src/hippocampus.h
+++ neural_engine/neural_engine/src/hippocampus.h
@@ -6,12 +6,14 @@ class hippocampus
 public:
 	struct grid_cells {
 		double V;
-	};
+	}; // NOTE: struct not in use currently
 	int GRID_POPULATION_NUMBER = 6;
 	int GRID_POPULATION_SIZE = 25;
+	//int active_grid_cell[2] = {{0,0}};
+
     hippocampus();
     ~hippocampus();
-    double** create_grid_population();
+    hippocampus::grid_cells** create_grid_population();
 
 };
 
