cf785cb2377142be980dc2546b1c7a412132acec
nmsutton <Nate Sutton>
2017-07-25 22:44:55 -0400

    step controlled differential equation working

--- neural_engine/neural_engine/src/hippocampus.cpp
+++ neural_engine/neural_engine/src/hippocampus.cpp
@@ -32,9 +32,10 @@ void rhs( const double x , double &dxdt , const double t )
     dxdt = 3.0/(2.0*t*t) + x/(2.0*t);
 }
 
-void sys( const hippocampus::state_type & /*x*/ , hippocampus::state_type & /*dxdt*/ , const double /*t*/ )
+void sys( const hippocampus::state_type &x , hippocampus::state_type &dxdt , const double t )
 {
-    // ...
+	//dxdt = 3.0 / (2.0*t*t) + x / (2.0*t);
+	dxdt[0] = x[1];
 }
 
 void write_cout( const double &x , const double t )
@@ -44,17 +45,9 @@ void write_cout( const double &x , const double t )
 
 void time_step() {
 	runge_kutta4< hippocampus::state_type > rk;
-	//double x = 0.0;
 	hippocampus::state_type x;
-	//typedef runge_kutta_dopri5< double > stepper_type;
-	//typedef boost::array< double , 3 > state_type;
-	//runge_kutta4< boost::array<double, 3> > rk;
-	//typedef boost::array<double, 3> state_type_2;
-	//runge_kutta4< state_type_2 > rk_2;
 	double t = 0;
-	//hippocampus::state_type t;
 	const double dt = 0.01;
-	//rk_2.do_step( rhs , x , t , dt );
 	rk.do_step( sys , x , t , dt );
 
 	//integrate_adaptive( make_controlled( 1E-12 , 1E-12 , stepper_type() ) , rhs , x , 1.0 , 10.0 , 0.1 , write_cout );
