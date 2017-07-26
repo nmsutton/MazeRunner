75ac034b4704ce1d8b84b6dbc1a2657c0d087899
nmsutton <Nate Sutton>
2017-07-25 22:21:48 -0400

    step controlled differential equation working

--- neural_engine/neural_engine/src/hippocampus.cpp
+++ neural_engine/neural_engine/src/hippocampus.cpp
@@ -1,6 +1,7 @@
 #include "hippocampus.h"
 
 using namespace std;
+using namespace boost::numeric::odeint;
 
 hippocampus::hippocampus()
 {
@@ -26,3 +27,35 @@ hippocampus::grid_cells** hippocampus::hippocampus::create_grid_population()
 	return grid_population;
 }
 
+void rhs( const double x , double &dxdt , const double t )
+{
+    dxdt = 3.0/(2.0*t*t) + x/(2.0*t);
+}
+
+void sys( const hippocampus::state_type & /*x*/ , hippocampus::state_type & /*dxdt*/ , const double /*t*/ )
+{
+    // ...
+}
+
+void write_cout( const double &x , const double t )
+{
+    cout << t << '\t' << x << endl;
+}
+
+void time_step() {
+	runge_kutta4< hippocampus::state_type > rk;
+	//double x = 0.0;
+	hippocampus::state_type x;
+	//typedef runge_kutta_dopri5< double > stepper_type;
+	//typedef boost::array< double , 3 > state_type;
+	//runge_kutta4< boost::array<double, 3> > rk;
+	//typedef boost::array<double, 3> state_type_2;
+	//runge_kutta4< state_type_2 > rk_2;
+	double t = 0;
+	//hippocampus::state_type t;
+	const double dt = 0.01;
+	//rk_2.do_step( rhs , x , t , dt );
+	rk.do_step( sys , x , t , dt );
+
+	//integrate_adaptive( make_controlled( 1E-12 , 1E-12 , stepper_type() ) , rhs , x , 1.0 , 10.0 , 0.1 , write_cout );
+}
--- neural_engine/neural_engine/src/hippocampus.h
+++ neural_engine/neural_engine/src/hippocampus.h
@@ -1,12 +1,24 @@
 #ifndef HIPPOCAMPUS_H
 #define HIPPOCAMPUS_H
 
+#include <iostream>
+#include <boost/array.hpp>
+
+#include <boost/numeric/odeint.hpp>
+
+using namespace std;
+using namespace boost::numeric::odeint;
+
 class hippocampus
 {
 public:
+	typedef boost::array<double, 3> state_type;
+	runge_kutta4< state_type > rk;
+
 	struct grid_cells {
 		double V;
-	}; // NOTE: struct not in use currently
+		//runge_kutta4< boost::array<double, 3> > rk;
+	};
 	int GRID_POPULATION_NUMBER = 6;
 	int GRID_POPULATION_SIZE = 25;
 	//int active_grid_cell[2] = {{0,0}};
@@ -14,6 +26,9 @@ public:
     hippocampus();
     ~hippocampus();
     hippocampus::grid_cells** create_grid_population();
+    void time_step();
+    void rhs();
+    void write_cout();
 
 };
 
